f242b3a8227719e1fe3cbffea0c621985fab0ca3
Ed Kubiak <ek33450505@gmail.com>
2022-06-19 19:31:58 -0400

    Nav bar created with spacing issues

---
+++ src/components/Nav/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+function Nav() {
+
+    return (
+        <header>
+        <h2>
+            <a href="/">
+            <span>Edward Kubiak</span>
+            </a>
+        </h2>
+        <nav>
+            <ul className="flex-row">
+            <li className="mx-2">
+                <a href="#about">
+                About me
+                </a>
+            </li>
+            <li>
+                <span>Contact</span>
+            </li>
+            <li>
+                <span>Portfolio</span>
+            </li>
+            <li>
+                <span>Resume</span>
+            </li>
+            </ul>
+        </nav>
+        </header>
+    );
+}
+
+export default Nav;
