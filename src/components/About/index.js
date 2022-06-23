3f5be65b61deb9ac6280fbf81a8b928f7e411256
Ed Kubiak <ek33450505@gmail.com>
2022-06-23 14:10:38 -0400

    initial set up of projects with new images in assets folder

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <section className="my-5">
             <h1 id="about">About Me</h1>
-                <h5>My name is Edward Kubiak. I am a Full Stack MERN developer currently living in Columbus, Ohio. Check out my portfolio and resume to learn more about me and my work!</h5>
+                <h5>My name is Edward Kubiak. I am a Full Stack MERN developer currently living in Columbus, Ohio. I love learning new technologies and developing applications that enhance the user experience. Check out my portfolio and resume to learn more about me and my work!</h5>
                     <img src={coverImage} class="center" className="my-2" alt="cover" />
         </section>
     );
---
+++ src/components/Project/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Project() {
+    return (
+        <Section>
+
+        </Section>
+    );
+}
+
+export default Project;
